0c5a9bdeebf0e42e9c2f2e569f91b9a1b3240959
Виктор Овсиенко <ovsienko@trueconf.ru>
2022-11-13 21:18:49 +0300

    Add groups, roles, permissions_to_users_objects

--- database/init.sql
+++ database/init.sql
@@ -13,27 +13,75 @@ create table if not exists main.users
     deleted_at   timestamptz
 );
 
-insert into main.users (display_name, login, hash) values ('Administrator', 'admin', '$2a$10$tjATGo3v5KrXQ6.cQz1CbugeBKRyJdmDbwr20rFMtzVJxOHtw3EIi');
+insert into main.users (display_name, login, hash)
+values ('Administrator', 'admin', '$2a$10$tjATGo3v5KrXQ6.cQz1CbugeBKRyJdmDbwr20rFMtzVJxOHtw3EIi');
 
 
 create table if not exists main.reports
 (
-    id         uuid primary key     default gen_random_uuid(),
-    title      varchar,
-    date       timestamptz not null,
-    start_time timestamp,
-    end_time   timestamp,
-    break_time timestamp,
-    work_time  timestamp,
-    body       varchar,
-    creator_id uuid references main.users (id),
-    created_at timestamptz not null default now(),
-    updated_at timestamptz not null default now(),
-    deleted_at timestamptz
+    id           uuid primary key     default gen_random_uuid(),
+    display_name varchar,
+    date         timestamptz not null,
+    start_time   timestamp,
+    end_time     timestamp,
+    break_time   timestamp,
+    work_time    timestamp,
+    body         varchar,
+    creator_id   uuid references main.users (id),
+    created_at   timestamptz not null default now(),
+    updated_at   timestamptz not null default now(),
+    deleted_at   timestamptz
 );
 
 create table if not exists main.reports_to_users
 (
     report_id uuid not null references main.reports (id),
     user_id   uuid not null references main.users (id)
+);
+
+
+create table if not exists main.groups
+(
+    id           uuid primary key     default gen_random_uuid(),
+    display_name varchar,
+    creator_id   uuid references main.users (id),
+    created_at   timestamptz not null default now(),
+    updated_at   timestamptz not null default now(),
+    deleted_at   timestamptz
+);
+
+create table if not exists main.groups_to_objects
+(
+    group_id    uuid references main.groups (id),
+    object_type varchar,
+    object_id   uuid
+);
+
+
+create table if not exists main.roles
+(
+    id varchar primary key,
+    description varchar
+);
+insert into main.roles (id, description)
+values ('administrator', null);
+
+create table if not exists main.users_to_roles
+(
+    user_id uuid    not null references main.users (id),
+    role_id varchar not null references main.roles (id),
+    primary key (user_id, role_id)
+);
+
+
+insert into main.users_to_roles (user_id, role_id)
+values ((select id from main.users where display_name = 'Administrator'),
+        (select id from main.roles where id = 'administrator'));
+
+
+create table if not exists main.permissions_users_to_objects
+(
+    user_id     uuid not null references main.users (id),
+    object_type varchar,
+    object_id   uuid
 );
